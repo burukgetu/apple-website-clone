5666de080df2f1b82c57a52f04be061bae70de5d
burukgetu <burukgetu000@gmail.com>
2024-09-27 08:50:51 -0300

    final frontend and 3d

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This allows external access (from your Android)
+    port: 3000,      // Use the port you want
+  },
+})
